fdcdb864249d430500c37d294cbebb29d3c6ba9c
harshkuche23 <harshkuche23@gmail.com>
2026-02-04 23:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function () {
+    const selectedIndex = select.selectedIndex;
+
+    if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+    }
+});
